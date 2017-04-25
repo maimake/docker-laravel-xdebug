dcb4136c9e58673dd73b202a499b82d4fe8bc383
mai <yshxinjian@gmail.com>
2017-04-25 20:37:47 +0800

    ll

--- install-php-ext.sh
+++ install-php-ext.sh
@@ -3,26 +3,6 @@
 set -ex
 
 
-apk add --no-cache --virtual .build-deps autoconf \
-file \
-g++ \
-gcc \
-binutils \
-isl \
-libatomic \
-libc-dev \
-musl-dev \
-make \
-re2c \
-libstdc++ \
-libgcc \
-binutils-libs \
-mpc1 \
-mpfr3 \
-gmp \
-libgomp
-
-
 
 pecl install xdebug
 docker-php-ext-enable xdebug
@@ -35,7 +15,3 @@ xdebug.remote_connect_back = 0
 xdebug.idekey = PHPSTORM
 
 EOF
-
-
-apk del .build-deps
-rm -rf /var/cache/apk/*
